f565aae366902da48fc6b14cbea30a6df620c695
Raja Hussnain Ali <hussnainraja768@gmail.com>
2025-02-27 17:06:58 +0500

    refactor: standardize quote usage and improve import organization in Home component

--- src/modules/Website/Home/Home.component.tsx
+++ src/modules/Website/Home/Home.component.tsx
@@ -1,19 +1,20 @@
-"use client";
+'use client';
 
-import Link from "next/link";
-import { useState } from "react";
+import Link from 'next/link';
+import { useState } from 'react';
 
+import { PlaceHomeCityProps } from '@ffp-web/app/index.types';
 import {
   Carousel,
   CategoriesSlider,
   CategoriesTab,
   PlacesSlider,
   SectionTitle,
-} from "@ffp-web/components";
+} from '@ffp-web/components';
+import SearchFilterModal from '@ffp-web/components/SearchFilterModal';
 
-import useHome from "./useHome";
-import SearchFilterModal from "@ffp-web/components/SearchFilterModal";
-import { PlaceHomeCityProps } from "@ffp-web/app/index.types";
+
+import useHome from './useHome';
 
 const HomePage = () => {
   const {
@@ -84,7 +85,7 @@ const HomePage = () => {
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 512 512"
             height="12"
-            style={{ paddingRight: "5px" }}
+            style={{ paddingRight: '5px' }}
           >
             <path d="M3.9 54.9C10.5 40.9 24.5 32 40 32l432 0c15.5 0 29.5 8.9 36.1 22.9s4.6 30.5-5.2 42.5L320 320.9 320 448c0 12.1-6.8 23.2-17.7 28.6s-23.8 4.3-33.5-3l-64-48c-8.1-6-12.8-15.5-12.8-25.6l0-79.1L9 97.3C-.7 85.4-2.8 68.8 3.9 54.9z" />
           </svg>
@@ -136,7 +137,7 @@ const HomePage = () => {
       <div className="card mx-8 md:mx-20 xl:mx-28 p-8 md:p-12 xl:p-16 bg-primary-bg rounded-xl flex flex-wrap gap-4 justify-center xl:justify-between items-center">
         <div className="text-center xl:text-left text-primary-white">
           <div className="text-xl md:text-3xl font-semibold">
-            Still not sure?{" "}
+            Still not sure?{' '}
             <span className="text-secondary-btn">Let us help you!</span>
           </div>
           <div className="text-base md:text-lg font-normal mt-3 md:mt-4">
