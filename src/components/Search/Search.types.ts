54b56b329f6754eb322e7542928494ac703271e8
Raja Hussnain Ali <hussnainraja768@gmail.com>
2025-02-27 17:13:46 +0500

    refactor: add suggestions property to UseSearch type definition

--- src/components/Search/Search.types.ts
+++ src/components/Search/Search.types.ts
@@ -8,4 +8,5 @@ export type UseSearch = {
   onChange: (e: ChangeEvent<HTMLInputElement>) => void,
   onKeyDown: (e: KeyboardEvent<HTMLInputElement>) => void,
   defaultQuery: string;
+  suggestions: { id: string; suggestion: string; type: string; detail?: string; suggestion_2?: string; suggestion_3?: string }[];
 };
